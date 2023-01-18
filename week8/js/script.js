4925bf772a19065d1bc7eb56f19f5549936955a0
Susav10 <np03cs4a220233@heraldcollege.edu.np>
2023-01-18 10:09:21 +0545

    weelk 6 and 7

---
+++ week8/js/script.js
@@ -0,0 +1,25 @@
+// alert("Hi I am alert");
+// console.log("Hi I am susav");
+// console.log("Hi I am 19 years old");
+// console.log(document.getElementById("paragraph").innerHTML)
+// document.getElementById("paragraph").innerHTML="<h1>Basantapur</h1>"
+// var susavaryal="HI I am susav aryal";
+// console.log(susavaryal);
+// document.getElementById("my_message").innerHTML="yahoo"+""+susavaryal+"!"
+
+// var age;
+// var first_name="susav";
+// var last_name="aryal";
+// var salary=500000;
+// var phone_number="9802410911"
+// console.log(age);
+
+function sum(a,b){
+    // var a=10;
+    // var b=20;
+    var sum=a+b;
+    console.log(sum);
+}
+sum(10,20);
+sum(20,30);
+sum(10,10)
