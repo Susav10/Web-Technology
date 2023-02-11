4b027b1d39200c9589792685835518e643e19f45
Susav10 <np03cs4a220233@heraldcollege.edu.np>
2023-02-11 11:24:04 +0545

    Protfolio Website

---
+++ week10/js/query.js
@@ -0,0 +1,5 @@
+var select = document.querySelectorAll("p.demo");
+
+for(var i=0;i<select.length;i++){
+    select[i].style.backgroundColor="green";
+}
---
+++ week10/js/variables.js
@@ -0,0 +1,22 @@
+// differents way of variables declaration
+let naam = "Susav";
+namm= "Rajib";
+
+let naam1="Mallik";
+namm1="Ushan";
+
+
+let naam2 ="Basanta";
+// namm2="Ryan";
+
+console.log(typeof naam);
+console.log(typeof naam1);
+console.log(typeof naam2);
+
+let number = prompt("Enter a number:");
+console.log(number);
+// document.getElementsByClassName("paragraph")[0].style.backgroundColor="blue";
+
+document.getElementsByClassName("paragraph")[number].style.cssText=" color:white; font-size:60px; ";
+document.getElementsByClassName("paragraph")[2].innerHTML="Hi this is week 10!!!";
+document.getElementsByTagName("h1")[1].innerHTML="DOM getElementsByTagName example";
